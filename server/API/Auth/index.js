9818bb18273c7db71eb7a9ea76c39acc9b41d237
diya-11 <lohardivya12@gmail.com>
2021-08-26 15:20:23 +0530

    add statics and methods, refactor signup route

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -18,38 +18,11 @@ Method    POST
 
 Router.post("/signup", async (req, res) => {
     try{
-        const { email, password, fullname, phoneNumber } = req.body.credentials;
-
-        // check wheather email exist
-        const checkUserByEmail = await UserModel.findOne({ email });
-        const checkUserByPhone = await UserModel.findOne({ phoneNumber });
-
-
-        if (checkUserByEmail || checkUserByPhone ) {
-            return res.json({ error: "User already exist!" });
-        }
-
-        // hash password
-        const bcryptSalt = await bcrypt.genSalt(8);
-
-        const hashedPassword = await bcrypt.hash(password, bcryptSalt);
-
-        // save to DB
-        await UserModel.create({
-             ...req.body.credentials,
-             password: hashedPassword,
-         });
-
-        // hash the password
-
-        // generate JWT auth token
-        const token = jwt.sign({ user: { fullname, email } }, "ZomatoAPP");
-
-
-        // return
+        await UserModel.findByEmailAndPhone(req.body.credentials);
+        const newUser = await UserModel.create(req.body.credentials);
+        const token = newUser.generateJwtToken();
         return res.status(200).json({ token, status: "success" });
-
-    } catch (error) {
+      } catch (error) {
         return res.status(500).json({ error: error.message });
     }
 });
