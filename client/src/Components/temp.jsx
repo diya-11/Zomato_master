3935bc53f1fc5c3d2f4a1b4a6a18c00fe4da7df6
diya-11 <lohardivya12@gmail.com>
2021-10-09 12:38:30 +0530

    src components added

--- client/src/Components/temp.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-
-const Temp = () => {
-  return <div>Hello this is a temp component</div>;
-};
-
-export default Temp;
--- server/API/image/index.js
+++ server/API/image/index.js
@@ -1,48 +1,50 @@
-// Libraries
+//Libraries
+//require('dotenv').config();
 import express from "express";
-import passport from "passport";
 import AWS from "aws-sdk";
 import multer from "multer";
 
-// Database modal
-import { ImageModel } from "../../database/allModels";
+//Database model
+import {ImageModel} from "../../database/allModels";
 
-// Utils
-import { s3Upload } from "../../Utils/AWS/s3";
+//Utilities
+import {s3Upload} from "../../Utils/AWS/s3";
 
 const Router = express.Router();
 
-// Multer config
+//Multer config
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-
+const upload = multer({storage});
 
 /*
-Route     /image
-Des       upload given image to S3 bucket, and return saves file link to mongodb
-Params    none
-Access    Public
-Method    POST
+Route            /
+Des              Uploading given image to S3 bucket , and then saving the file to mongodb
+Params           None
+Access           Public
+Method           POST
 */
-Router.post("/", upload.single("file"), async (req, res) => {
-    try{
-        const file = req.file;
 
-        const bucketOptions = {
-            Bucket: "zomatoawsbucket",
-            Key: file.originalname,
-            Body: file.buffer,
-            ContentType: file.minetype,
-            ACL: "public-read",
-        };
+Router.post("/", upload.single("file") ,async(req,res)=> {
+  try {
+ const file = req.file;
 
-        const uploadImage = await s3Upload(bucketOptions);
+ //S3 bucket options
+ const bucketOptions = {
+   Bucket: "zomatoawsbucket",
+   Key: file.originalname,
+   Body: file.buffer,
+   ContentType: file.mimetype,
+   ACL: "public-read"
+ };
 
-        return res.status(200).json({ uploadImage: "yay" });
-    }catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
+
+ const uploadImage = await s3Upload(bucketOptions);
+
+ return res.status(200).json({ uploadImage });
+
+  } catch (error) {
+return res.status(500).json({error: error.message});
+  }
 });
 
 export default Router;
