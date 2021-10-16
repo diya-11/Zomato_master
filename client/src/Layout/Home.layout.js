4cc57fc7e9b9e29962659764b41fc6d044a9911e
diya-11 <lohardivya12@gmail.com>
2021-10-16 15:21:30 +0530

    done

--- client/src/Layout/Home.layout.js
+++ client/src/Layout/Home.layout.js
@@ -1,28 +1,24 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch } from "react-router";
-//Components
+import React,{useEffect} from "react";
+import {useDispatch} from "react-redux";
+
 import Navbar from "../Components/Navbar";
 import FoodTab from "../Components/FoodTab";
-
-//redux actions
 import { getRestaurant } from "../Redux/Reducer/restaurant/restaurant.action";
 
 const HomeLayout = (props) => {
-const dispatch = useDispatch();
+ const dispatch = useDispatch();
 
-useEffect(()=> {
-  dispatch(getRestaurant());
-}, []);
+ useEffect(() => {
+   dispatch(getRestaurant());
+ }, []);
 
-  return <>
-   <div className="container mx-auto lg:px-20">
-   <Navbar />
-
+    return <>
+     <div className="container mx-auto lg:px-20">
+         <Navbar />
     {props.children}
-   </div>
-   <FoodTab />
-  </>
-};
-
-export default HomeLayout;
+    </div>
+    <FoodTab/>
+    </>
+  };
+  
+  export default HomeLayout;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-
 import {Provider} from "react-redux";
 import Store from "./Redux/store";
 
+
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-  <BrowserRouter>
+    <Provider store={Store}>
+    <BrowserRouter>
     <App />
     </BrowserRouter>
-  </Provider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
